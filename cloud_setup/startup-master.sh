fd5000c63ca09f0e112530655868af123cb1567c
Manish Vankudre <82750764+ManishV18@users.noreply.github.com>
2024-12-03 08:09:08 -0500

    Create startup-master.sh

---
+++ cloud_setup/startup-master.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update package list and install dependencies
+apt-get update
+apt-get install -y python3-pip git
+
+# Upgrade pip
+pip3 install --upgrade pip
+
+# Install project-specific dependencies for the master node
+pip3 install google-cloud-storage google-cloud-pubsub flask whisper transformers
+
+# Clone the project repository
+PROJECT_REPO="git@github.com:ManishV18/fp1.git"
+PROJECT_DIR="/home/ubuntu/fp1"
+git clone "$PROJECT_REPO" "$PROJECT_DIR"
+
+# Navigate to the master directory and install Python dependencies
+cd "$PROJECT_DIR/master"
+pip3 install -r requirements.txt
+
+# Run the master script
+python3 master.py
