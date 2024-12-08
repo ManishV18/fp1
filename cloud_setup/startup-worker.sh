68a615028556bd811675c99a81ad10247e267d2c
Manish Vankudre <82750764+ManishV18@users.noreply.github.com>
2024-12-07 19:05:06 -0500

    Update startup-worker.sh

--- cloud_setup/startup-worker.sh
+++ cloud_setup/startup-worker.sh
@@ -11,7 +11,7 @@ pip3 install --upgrade pip
 pip3 install google-cloud-storage google-cloud-pubsub whisper transformers
 
 # Clone the project repository
-PROJECT_REPO="https://github.com/ManishV18/dcsc_final_project.git"
+PROJECT_REPO="https://github.com/ManishV18/cu-csci-4253-datacenter-fall-2024 / finalproject-final-project-team-89.git"
 PROJECT_DIR="/home/ubuntu/fp1"
 git clone "$PROJECT_REPO" "$PROJECT_DIR"
 
