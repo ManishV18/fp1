765af6f1ed07e8c372b04751537567b45b795ada
Manish Vankudre <82750764+ManishV18@users.noreply.github.com>
2024-12-07 19:04:15 -0500

    Update startup-master.sh

--- cloud_setup/startup-master.sh
+++ cloud_setup/startup-master.sh
@@ -11,7 +11,7 @@ pip3 install --upgrade pip
 pip3 install google-cloud-storage google-cloud-pubsub flask whisper transformers
 
 # Clone the project repository
-PROJECT_REPO="https://github.com/ManishV18/fp1.git"
+PROJECT_REPO="https://github.com/ManishV18/dcsc_final_project.git"
 PROJECT_DIR="/home/ubuntu/fp1"
 git clone "$PROJECT_REPO" "$PROJECT_DIR"
 
